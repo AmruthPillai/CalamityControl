8f7fc0d5ec2309d860093eaf79a8ca96b496ebd8
Rohan Kumar <rohan.carmel@gmail.com>
2016-12-12 15:49:30 +0530

    upload emergency

---
+++ emergency-upload/firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCMoO7CX52RaO5CqSBWTZ67PiLiigAh4jM",
+    authDomain: "calamity-control-1478121312942.firebaseapp.com",
+    databaseURL: "https://calamity-control-1478121312942.firebaseio.com",
+    storageBucket: "calamity-control-1478121312942.appspot.com",
+    messagingSenderId: "552005603735"
+  };
+
+var firebaseapp = firebase.initializeApp(config);
+var firebasedb = firebase.database();
